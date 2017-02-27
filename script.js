63b6773188fbf27c9bb5c57b3732c9f2dd0d0c7e
Victor Moretti <victormorettir@gmail.com>
2017-02-27 13:56:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 var lista = document.getElementById('lista'); //crie um <pre id="lista"> tag para sair como parágrafos em Mardown
 var tags = document.getElementsByTagName('a');
-var link = [];
-var title = [];
 for (index of tags) {
-    link.push(index.href);
-    title.push(index.innerHTML);
-}
-for(var i=0; i < link.length; i++) {
-    lista.innerHTML += '['+title[i]+']'+'('+link[i]+')\r\n\r\n';
+    lista.innerHTML += '['+index.innerHTML+']'+'('+index.href+')\r\n\r\n';
 }
